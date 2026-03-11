4053659a0fc4fd488c8e087bdc2c9e201ec6cbd9
Kun Xi <kunxi@kunxi.org>
2026-03-11 10:43:54 -0700

    Add wslinterop fix.

--- zsh/default.nix
+++ zsh/default.nix
@@ -84,6 +84,12 @@
         alias pbcopy=clip.exe
         # use native ssh-agent
         eval $(ssh-agent -s)
+
+        # Fix the WSLInterop broken issue
+        if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+          sudo mount -t binfmt_misc none /proc/sys/fs/binfmt_misc 2>/dev/null
+          echo ':WSLInterop:M::MZ::/init:PF' | sudo tee /proc/sys/fs/binfmt_misc/register >/dev/null
+        fi
       fi
     '';
   };
